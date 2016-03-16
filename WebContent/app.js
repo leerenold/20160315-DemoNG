7b7b6826712d8efd4769e37dbc365e0992fc6bcc
calvinvette <calvin.vette@nextgeneducation.com>
2016-03-16 11:02:05 -0700

    Converted to CustomerLocalStorageService
    Added CLSS, migrated code from app.js to it
    Tweaked CustomerTableController and index.html to adapt.

--- WebContent/app.js
+++ WebContent/app.js
@@ -1,15 +1,15 @@
 var workingCustomer;
-//var customers = [];
+// var customers = [];
 
 
-//var app = angular.module("DemoNG", []);
+// var app = angular.module("DemoNG", []);
 angular.module("DemoNG", ['ngRoute'])
-	//.config(...)
+	// .config(...)
 	.config(function($routeProvider, $locationProvider) {
 		$routeProvider
 		.when("/", {
 			templateUrl: 'main',
-			//controller: 'MainController'
+			// controller: 'MainController'
 		})
 		.when("/CustomerForm", {
 			templateUrl: 'CustomerForm'
@@ -20,31 +20,18 @@ angular.module("DemoNG", ['ngRoute'])
 		.otherwise({
 			redirectTo: "/#"
 		})
-		//$locationProvider.html5Mode(true);
+		// $locationProvider.html5Mode(true);
 	})
-	.run(function($rootScope) { // $rootScope is really the "app"
+	.run(["$rootScope", "CustomerLocalStorageService", function($rootScope, CustomerLocalStorageService) { // $rootScope is really the "app"
 		console.log("Started...");
 		
-		$rootScope.customers = [
-		                        new Customer(1, "Harry", "Potter", "+44 0206 555-1212", "harry.potter@hogwarts.ac.uk"),
-		                        new Customer(2, "Ron", "Weasley", "+44 0206 555-1213", "ron@hogwarts.ac.uk"),
-		                        new Customer(3, "Hermione", "Granger", "+44 0206 555-1512", "hermione@hogwarts.ac.uk"),
-		                        new Customer(4, "Neville", "Longbottom", "+44 0206 555-1513", "neville@hogwarts.ac.uk"),
-		                        
-		                        ];
-		$rootScope.lastCustomerId = 0;
-		
 		$rootScope.$on("CustomerAddedEvent", function(evt, cust) {
-			console.log(cust);
-			cust.customerId = ++$rootScope.lastCustomerId; // implicit creation first time with val=0
-			$rootScope.customers.push(cust);
+			CustomerLocalStorageService.addCustomer(cust);
 		});
-	})
-	//.controller(...)
-
-
-
-
+		
+		
+	}])
+	// .controller(...)
 
 
 ;
